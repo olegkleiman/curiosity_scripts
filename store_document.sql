6dcc834e89da74d91ce76cc100941f340a45115d
Oleg Kleiman <oleg_kleyman@hotmail.com>
2023-09-05 11:55:39 +0300

    Update store_document.sql

--- store_document.sql
+++ store_document.sql
@@ -1,11 +1,1 @@
-CREATE PROCEDURE [dbo].[storeDocument]
-	@doc nvarchar(max),
-	@title nvarchar(max),
-	@url nvarchar(max)
-AS
-	insert into [dbo].[site_docs]
-	(doc, title, url)
-	values(@doc, @title, @url)
 
-	return SCOPE_IDENTITY()
-GO
