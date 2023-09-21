43c693c850e40c9ebe728f418991359bf2bb09e2
Oleg Kleiman <oleg_kleyman@hotmail.com>
2023-09-21 20:06:15 +0300

    Update store_document_sp.sql

--- store_document_sp.sql
+++ store_document_sp.sql
@@ -1,11 +1,12 @@
 CREATE PROCEDURE [dbo].[storeDocument]
 	@doc nvarchar(max),
 	@title nvarchar(max),
-	@url nvarchar(max)
+	@url nvarchar(max),
+	@geom_location nvarchar(max)
 AS
 	insert into [dbo].[site_docs]
-	(doc, title, url)
-	values(@doc, @title, @url)
+	(doc, title, url, [latlong])
+	values(@doc, @title, @url, geography::STGeomFromText(@geom_location, 4326))
 
 	return SCOPE_IDENTITY()
 GO
