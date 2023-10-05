a7dfb5f8e1702793c1f397aff900b33861f646d3
Oleg Kleiman <oleg_kleyman@hotmail.com>
2023-10-05 23:28:06 +0300

    Update and rename docs_table.sql to site_docs.sql

--- site_docs.sql
+++ site_docs.sql
@@ -1,8 +1,18 @@
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 CREATE TABLE [dbo].[site_docs](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[doc] [nvarchar](max) NOT NULL,
 	[title] [nvarchar](max) NOT NULL,
 	[url] [nvarchar](max) NOT NULL,
+	[address] [nvarchar](max) NULL,
+	[location] [geography] NULL,
+	[starts] [datetime] NULL,
+	[ends] [datetime] NULL,
  CONSTRAINT [PK__site_doc__3214EC0791352496] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
